8e9169f67c3cac0077c61c7f5c20d59bba471200
Brent Porter <eporter2@live.maryville.edu>
2019-12-02 03:13:00 -0700

    Recreated Project on ionic 3 installation

---
+++ GroceriesApp/src/pages/home/home.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public InputDialogService: InputDialogServiceProvider) {
+
+  }
+
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.dataService.removeItem(index);
+
+  }
+
+  editItem(item, index) {
+    console.log("Editing an Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Editing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present(),
+      this.InputDialogService.showPrompt(item, index);
+
+  }
+
+  addItem() {
+    console.log("Adding an item to list");
+    this.InputDialogService.showPrompt();
+  }
+
+}
